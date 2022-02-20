5938ee4f592d89b77435d803773f3f2a08754e4b
oat431 <oat431@gmail.com>
2022-02-20 15:09:49 +0700

    [🍀 prepare] : for workshop

--- lib/service/covid_news_api.dart
+++ lib/service/covid_news_api.dart
@@ -1,23 +1,25 @@
 import 'dart:async';
+import 'package:intl/intl.dart';
 import 'package:http/http.dart' as http;
 
 class CovidNewsApi {
-
+  var now = DateTime.now();
+  var formatter = DateFormat('yyyy-MM-dd');
   final String url = "newsapi.org";
   final String path = "/v2/everything";
   final String query = "COVID";
-  final String date = "2022-02-01";
   final String sortBy = "date";
   final String apiKey = "52416b411fea44b1be07faf152d9f178";
 
   Future<http.Response> getCovidNews(){
+    print(formatter.format(now));
     return http.get(
         Uri.https(
             url,
             path,
             {
               'q' : query,
-              'from' : date,
+              'from' : formatter.format(now),
               'sortBy' : sortBy,
               'apiKey' : apiKey
             }
